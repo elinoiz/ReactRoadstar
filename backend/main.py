a0a8e815ec67ee5b7410dda21c95826b0f2aa91a
elinoiz <saleks_023@mail.ru>
2024-06-20 22:15:37 +0700

    Change main.py

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
